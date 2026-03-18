8627fd0daef72908e95d80214fe34dbe1de43c87
by-creator <marcdamien04@gmail.com>
2026-03-18 07:15:00 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/demat', 'demat.index')->name('demat');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
