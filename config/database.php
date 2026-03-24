c89bd8cea4486a40b5e1f0333ad33d4b8146f81a
by-creator <marcdamien04@gmail.com>
2026-03-24 10:12:32 +0000

    update

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_LOCAL_INFILE => env('MYSQL_ATTR_LOCAL_INFILE', true),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_LOCAL_INFILE : PDO::MYSQL_ATTR_LOCAL_INFILE) => env('MYSQL_ATTR_LOCAL_INFILE', true),
             ]) : [],
         ],
 
@@ -82,7 +82,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_LOCAL_INFILE => env('MYSQL_ATTR_LOCAL_INFILE', true),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_LOCAL_INFILE : PDO::MYSQL_ATTR_LOCAL_INFILE) => env('MYSQL_ATTR_LOCAL_INFILE', true),
             ]) : [],
         ],
 
